a0b89711482724eda13c69bbaa730f4b0e182665
jaeeuncho34 <jaylin.cho@klaytn.foundation>
2025-04-04 15:55:26 +0900

    update aws/private-l1-scn

--- terraform/aws/private-l1-scn/ansible-inventory.tf
+++ terraform/aws/private-l1-scn/ansible-inventory.tf
@@ -10,8 +10,28 @@ locals {
       monitor                      = try(module.layer1.monitor, {})
     }
   )
+  ansible_vars_all = templatefile(
+    "${path.module}/templates/groupvarsall.tftpl",
+    {
+      kaia_version = var.deploy_options.kaia_version
+      kaia_num_cn = var.cn_options.count
+      kaia_num_pn = var.pn_options.count
+      kaia_num_en = var.en_options.count
+      kaia_num_scn = var.scn_options.count
+      kaia_network_id = var.deploy_options.kaia_network_id
+      kaia_chain_id = var.deploy_options.kaia_chain_id
+      kaia_service_network_id = var.deploy_options.kaia_service_network_id
+      kaia_service_chain_id = var.deploy_options.kaia_service_chain_id
+    }
+  )
 }
+
 resource "local_file" "this" {
   content  = local.ansible_inventory
   filename = format("%s/../../../inventory/%s/inventory.ini", path.module, basename(abspath(path.module)))
+}
+
+resource "local_file" "ansible_group_vars_all" {
+  content  = local.ansible_vars_all
+  filename = format("%s/../../../inventory/%s/group_vars/all/all.yml", path.module, basename(abspath(path.module)))
 }
