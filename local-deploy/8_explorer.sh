d2449b3a9e4d440edbf90e5a4c0a11c547a1cc8d
ollie.j <ollie.j@klaytn.foundation>
2025-06-20 08:58:12 +0900

    local/explorer: Expose to outside localhost

--- local-deploy/8_explorer.sh
+++ local-deploy/8_explorer.sh
@@ -7,11 +7,11 @@ HOMEDIR="$(dirname "$0")"
 cd $HOMEDIR
 
 docker_compose() {
-	if docker compose version >/dev/null 2>&1; then
-		docker compose -f explorer/docker-compose.yml "$@"
-	else
-		docker-compose -f explorer/docker-compose.yml "$@"
-	fi
+    if docker compose version >/dev/null 2>&1; then
+        docker compose -f explorer/docker-compose.yml "$@"
+    else
+        docker-compose -f explorer/docker-compose.yml "$@"
+    fi
 }
 
 start_explorer() {
@@ -21,9 +21,11 @@ start_explorer() {
     WS_PORT=$(sed -n 's/^WS_PORT=\([0-9]*\)$/\1/p' ${1}${2}/conf/k${1}d.conf)
     WS_URL=ws://host.docker.internal:$WS_PORT
     CHAIN_ID=$(sed -n 's/^NETWORK_ID=\([0-9]*\)$/\1/p' ${1}${2}/conf/k${1}d.conf)
-    echo $RPC_URL $WS_URL $CHAIN_ID
+    HOST_DOMAIN=${3:-localhost}
+    echo "Leeching from ${1}${2}: RPC_URL=$RPC_URL WS_URL=$WS_URL CHIAN_ID=$CHAIN_ID"
+    echo "Listening to: http://$HOST_DOMAIN"
 
-    RPC_URL=$RPC_URL WS_URL=$WS_URL CHAIN_ID=$CHAIN_ID docker_compose up -d
+    RPC_URL=$RPC_URL WS_URL=$WS_URL CHAIN_ID=$CHAIN_ID HOST_DOMAIN=$HOST_DOMAIN docker_compose up -d
     set +e
 }
 
@@ -33,11 +35,15 @@ case "$1" in
         start_explorer "$@"
         ;;
     stop)
-        docker_compose stop
+        docker_compose down
         ;;
     *)
         echo "Usage: $0 {start|stop}"
+        echo
+        echo "  $0 start <rpc> <listen domain>"
+        echo "  $0 start en 1  mydomain.com"
         echo "  $0 start en 1"
+        echo
         echo "  $0 stop"
         exit 1
         ;;
