9c57bbebfeb614033646701749c6b60322b51936
yumiel <yumiel.ko@kaia.io>
2025-09-11 17:01:46 +0900

    aws: update ansible variables

--- terraform/aws/private-layer1/ansible-inventory.tf
+++ terraform/aws/private-layer1/ansible-inventory.tf
@@ -13,13 +13,24 @@ locals {
     "${path.module}/templates/groupvarsall.tftpl",
     {
       kaia_install_mode = var.deploy_options.kaia_install_mode
-      kaia_version      = var.deploy_options.kaia_version
+      kaia_version      = try(var.deploy_options.kaia_version, "")
       kaia_build_docker_base_image = var.deploy_options.kaia_build_docker_base_image
       kaia_num_cn       = var.cn_options.count
       kaia_num_pn       = var.pn_options.count
       kaia_num_en       = var.en_options.count
-      kaia_network_id   = var.deploy_options.kaia_network_id
-      kaia_chain_id     = var.deploy_options.kaia_chain_id
+      kaia_network      = try(var.deploy_options.kaia_network, "")
+      kaia_network_id   = try(var.deploy_options.kaia_network_id, "")
+      kaia_chain_id     = try(var.deploy_options.kaia_chain_id, "")
+      homi_extra_options_cn  = try(var.deploy_options.homi_extra_options.cn, "")
+      homi_extra_options_pn  = try(var.deploy_options.homi_extra_options.pn, "")
+      homi_extra_options_en  = try(var.deploy_options.homi_extra_options.en, "")
+      homi_extra_options_scn = try(var.deploy_options.homi_extra_options.scn, "")
+      homi_extra_options_spn = try(var.deploy_options.homi_extra_options.spn, "")
+      homi_extra_options_sen = try(var.deploy_options.homi_extra_options.sen, "")
+      cn_options        = jsonencode(var.cn_options)
+      pn_options        = jsonencode(var.pn_options)
+      en_options        = jsonencode(var.en_options)
+      deploy_options    = jsonencode(var.deploy_options)
     }
   )
 }
